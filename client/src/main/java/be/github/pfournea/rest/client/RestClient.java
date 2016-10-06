a0763cc6d28d69f5a201181003afc4a9150799f8
pfournea <peter.fourneau@gmail.com>
2016-10-06 21:24:27 +0200

    add first implementation for client

--- client/src/main/java/be/github/pfournea/rest/client/RestClient.java
+++ client/src/main/java/be/github/pfournea/rest/client/RestClient.java
@@ -1,13 +1,28 @@
 package be.github.pfournea.rest.client;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 /**
  * Created by Peter on 16/09/2016.
  */
-public class RestClient {
+public class RestClient<T> {
+    @Value("${restapi.zuulurl}")
+    private String zuulUrl;
+    @Value("${restapi.eurekaurl}")
+    private String eurekaUrl;
+
+    @Value("${restapi.mediatype}")
+    private String mediaType;
+
+    private Class<T> classOfT;
 
     private RestTemplate restTemplate;
 
@@ -16,6 +31,20 @@ public class RestClient {
     }
 
     public <T> T getById(UUID id,Class<T> type){
-        return null;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.valueOf(mediaType)));
+        HttpEntity httpEntity = new HttpEntity(headers);
+        UriComponents uriComponents = UriComponentsBuilder
+                .fromUriString(selectAppropriateUrl())
+                .path("/api/resource/")
+                .pathSegment("api", "resource", id.toString())
+                .build();
+        ResponseEntity<T> exchange = (ResponseEntity<T>) restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, httpEntity, classOfT);
+        return exchange.getBody();
+    }
+
+    private String selectAppropriateUrl() {
+        LoadBalanced[] declaredAnnotationsByType = restTemplate.getClass().getDeclaredAnnotationsByType(LoadBalanced.class);
+        return declaredAnnotationsByType.length > 0 ? eurekaUrl : zuulUrl;
     }
 }
