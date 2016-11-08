09302f3990e8142cf3cc52d8d70c14bde1f1f23e
pfournea <peter.fourneau@gmail.com>
2016-11-08 21:13:34 +0100

    add zuul service + add static routing zuul/google -> www.google.be

---
+++ zuul/src/main/java/be/github/pfournea/zuul/Application.java
@@ -0,0 +1,16 @@
+package be.github.pfournea.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by Peter on 26/09/2016.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
